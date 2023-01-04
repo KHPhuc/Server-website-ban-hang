2f7a546976e0a102d7c0513f167de81789a7922a
KP23 <kieuphuck17h@gmail.com>
2023-01-04 20:58:46 +0700

    U

--- build/routes/Product/product.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_controller_1 = require("../../controller/product.controller");
-const productRouter = (0, express_1.Router)();
-productRouter.get("/", product_controller_1.getAll);
-exports.default = productRouter;
