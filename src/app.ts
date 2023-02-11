5617deb5cd3a4b54a34b4f2b4d480ee1389e4560
KP23 <kieuphuck17h@gmail.com>
2023-02-11 14:20:46 +0700

    U

--- src/app.ts
+++ src/app.ts
@@ -52,9 +52,6 @@ app.get("/", (req, res) => {
 
 app.use("/api", router);
 
-// app.listen(process.env.PORT || config.port, () => {
-//   console.log(`Running on http://localhost:${config.port}`);
-// });
-app.listen(config.port, () => {
+app.listen(process.env.PORT || config.port, () => {
   console.log(`Running on http://localhost:${config.port}`);
 });
